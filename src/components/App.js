030ce00d9ce3818b4ba1a81753d84b996be2b899
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2021-03-01 18:38:54 -0600

    added basic navbar and title

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import '../App.css';
+import Navbar from './Navbar.js'
+
+function App() {
+  return (
+    <div className="App">
+  <h1>DENA
+    NEUMAN</h1>
+  <h3>Full Stack Software Engineer</h3>
+  <hr class='dotted'/>
+<Navbar/>
+    </div>
+  );
+}
+
+export default App;
